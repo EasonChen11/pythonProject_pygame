04ab02dda0ace6426a03c0ca182b6034581d224f
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-05-09 14:15:30 +0800

    sprite 飛船

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((50, 40))   # 創造平面
         self.image.fill((0, 255, 0))    # 設定平面顏色
         self.rect = self.image.get_rect()   # 把圖片加框線(可設定中心、上方...) (自己的邊界=圖片框線)(rect=rectangle矩形)
-        self.rect.x = 200                   # 設定座標位
-        self.rect.y = 200
-        self.rect.center = (50, 40)
+        # self.rect.x = 200                   # 設定座標位
+        # self.rect.y = 200
+        self.rect.centerx  = WIDTH/2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 8
     def update(self):
         key_pressed = pygame.key.get_pressed()  # 取得鍵盤哪個鍵被按下的bool，函式會傳回list包含每個按鍵的bool(有按=True,else=False)
         if key_pressed[pygame.K_RIGHT]:
